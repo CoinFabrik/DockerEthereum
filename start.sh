9d5c3f46534f7a73bf73109afb91fc10650222a9
Leandro Rawicz <leandro.rawicz@coinfabrik.com>
2019-03-20 10:54:53 -0300

    estable

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-docker rm ethnet
+#docker rm ethnet
 docker build -t leanbase  ./base/
 docker build -t ethnet ./ethnetstat/
-docker build -t node1  ./PoW/
-docker build -t node2  ./PoW/
-#docker run -p 127.0.0.1:3000:3000 ethnet 
+docker build -t node  ./PoW/	
+#docker run -it -p 127.0.0.1:3000:3000 --name EthernetServer ethnet
+#docker run -it -p 127.0.0.1:8545:8545 -p 127.0.0.1:8000:8000  --name Node1 node 
 
 
 #docker exec -d ethnet cd eth-netstats && npm start
