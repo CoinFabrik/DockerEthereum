421f9dbf894f8192a10fccacdb9975b028995068
Leandro Rawicz <leandro.rawicz@coinfabrik.com>
2019-03-28 15:48:14 -0300

    estable resolver problema de DAG

--- PoW/Node2/node2.sh
+++ PoW/Node2/node2.sh
@@ -1,4 +1,5 @@
 geth --rpc --rpcport 8555 --rpcaddr "0.0.0.0" \
 	 --port 30503 --networkid 22 \
 	 --datadir="./node" --ipcpath="geth.ipc" \
-	 --rpccorsdomain "*" 
+	 --rpccorsdomain "*" \
+	 --nodiscover
