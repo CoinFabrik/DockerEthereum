9e13b9265f74b6d5049c58e3c64d224f55609d98
lrawiczNC <leandro.rawicz@coinfabrik.com>
2019-04-09 13:32:12 -0300

    getBalance

--- scripts/getBalance.sh
+++ scripts/getBalance.sh
@@ -1,9 +1,16 @@
+#!/bin/bash
+if [ -z "$1" ];
+then
+	account="web3.eth.accounts[0]"
+else
+	account='"'$1'"'
+fi
 attach=" | geth attach ipc:./node/geth.ipc"
 
 
- 	cmdNode="echo 'web3.eth.getBalance(web3.eth.accounts[0])'"
+cmdNode="echo 'web3.eth.getBalance($account)'"
 
-
+echo "$cmdNode $attach" 
 docker exec -t Node1 sh  -c "$cmdNode $attach" 
 
 
